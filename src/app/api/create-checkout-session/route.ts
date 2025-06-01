b6bc216ce61af5f24375db8f362287f77698c35f
friday ogboji <friday.ogboji100@gmail.com>
2025-06-01 13:59:45 +0100

    Ready for Vercel deploy

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -4,7 +4,7 @@ import { prisma } from "@/lib/prisma";
 
 // Initialize Stripe
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-04-30.basil',
+  apiVersion: '2025-05-28.basil',
 });
 
 export async function POST(req: NextRequest) {
