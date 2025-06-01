dab044d29d6d53400ecfc81a11cc78088e008b51
friday ogboji <friday.ogboji100@gmail.com>
2025-06-01 13:27:35 +0100

    Ready for Vercel deploy

--- src/app/api/reviews/count/[productId]/route.ts
+++ src/app/api/reviews/count/[productId]/route.ts
@@ -1,9 +1,9 @@
 // app/api/reviews/count/[productId]/route.ts
 
 import { NextResponse } from 'next/server';
-import {prisma} from '@/lib/prisma';
+import { prisma } from '@/lib/prisma';
 
-export async function GET(req: Request, { params }: { params: { productId: string } }) {
+export async function GET(req: Request, { params }: any) {
   const { productId } = params;
 
   const count = await prisma.review.count({
